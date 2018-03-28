c81cbdf1bd27f99ee2b037dd057e5b9f87fe74d4
Ryan Fadholi <rynfdhl@gmail.com>
2018-03-29 01:04:19 +0700

    Fixed file name used in main.py

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ import numpy as np
 from simplekmeans import kmeans
 
 k_value = 5
-datapoints = np.loadtxt("dataset-uts.txt")
-print(f"Type: {type(datapoints)}")
+datapoints = np.loadtxt("example.txt")
 kmeans(k_value, datapoints)
